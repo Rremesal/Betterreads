7e8947d06baf3941cdb4227bffae1c6bbcc15f06
Rremesal <34509994+Rremesal@users.noreply.github.com>
2023-01-23 15:16:52 +0100

    created the navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,7 +1,10 @@
-<nav>
-    <a href="{{ route('home') }}">Home</a>
-    <a href="">My Books</a>
-    <a href="">Browse</a>
-    <form action=""><input type="text"></form>
-    <a href="">Profile</a>
+<nav class=" bg-yellow-100 flex justify-center p-3 overflow-y-hidden ">
+    <div class="w-62 flex items-center h-7">
+        <a class=" py-24 px-5 mx-2 text-amber-900 hover:text-yellow-100 hover:bg-amber-900" href="{{ route('home') }}">Home</a>
+        <a class=" py-24 px-5 mx-2 text-amber-900 hover:text-yellow-100 hover:bg-amber-900" href="">My Books</a>
+        <a class="py-24 px-5 mx-2 text-amber-900 hover:text-yellow-100 hover:bg-amber-900" href="">Browse</a>
+        <form class="mx-2" action=""><input class="px-2" type="text" placeholder="Search.."></form>
+        <a class="py-24 px-5 mx-2 text-amber-900 hover:text-yellow-100 hover:bg-amber-900" href="">Profile</a>
+    </div>
+    
 </nav>
