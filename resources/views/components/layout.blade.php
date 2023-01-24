d4f32e428f31c7f715002ab18cc03ba2e9794142
Rremesal <34509994+Rremesal@users.noreply.github.com>
2023-01-24 11:02:23 +0100

    created the profile dropdown menu

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -12,4 +12,5 @@
     {{ $slot }}
     
 </body>
+<script src="{{ asset('js/script.js') }}"></script>
 </html>
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,3 +1,11 @@
 <x-layout>
 
+    <div class="flex flex-col w-52 items-center border-2 shadow-lg">
+        <span class="font-semibold">{{ Auth::user()->first_name." ".Auth::user()->last_name }}</span>
+        <a class="text-blue-400 hover:underline w-full" href="">Profile</a>
+        <a class="text-blue-400 hover:underline w-full" href="">Comments</a>
+        <a class="text-blue-400 hover:underline w-full" href="">Account settings</a>
+        <a class="text-blue-400 hover:underline border-t-2 w-full" href="">Log out</a>
+    </div>
+
 </x-layout>
